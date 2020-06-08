5c4173111435f3c9471dd61f198b13c6fd16ee16
Sam Dratch <dratch.sam1@gmail.com>
2020-06-08 16:40:01 -0400

    Moved the assignment to a new file and wrote a README file. Updated the code to prompt the user

--- Develop/script.js
+++ Develop/script.js
@@ -7,8 +7,70 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var lowerCase = [];
+  var upperCase = [];
+  var numbers = [];
+
+  //Create 2 arrays with one all lower case and one all upper case
+  for (var i = 65; i < 91; i++) {
+    upperCase.push(String.fromCharCode(i));
+    lowerCase.push(String.fromCharCode(i).toLowerCase());
+  }
+  //creat an array of numbers
+  for (var i = 48; i < 58; i++) {
+    numbers.push(String.fromCharCode(i));
+  }
+  //create special character array
+  var specialChar = [
+    "!",
+    '"',
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "]",
+    "\\",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+  var characterChoice = [1, 2, 3, 4];
+
+  //prompts the user for the required information
+  var passLength = prompt(
+    "Enter a number between 8 and 128 for how long you want the password to be"
+  );
+  var isLower = confirm("Do you want lower case letters?");
+  var isUpper = confirm("Do you want upper case letters?");
+  var isSpecial = confirm("Do you want special characters?");
+  var isNumber = confirm("Do you want numbers?");
+  
+
+}
