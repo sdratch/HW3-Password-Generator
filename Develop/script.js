cefeba8b14a3aa0bddd8f96eb0390f02281ac327
Sam Dratch <dratch.sam1@gmail.com>
2020-06-08 18:07:48 -0400

    completed details to return password and added more checks for errors

--- Develop/script.js
+++ Develop/script.js
@@ -63,87 +63,96 @@ function generatePassword() {
   ];
   //create an array for selecting characters
   var characterChoice = [];
-  var workingPass;
+  //create a working password that will be added onto to make the completed one
+  var workingPass = "";
 
   //prompts the user for the required information
   var passLength = prompt(
     "Enter a number of atleast 8 or atmost 128 for how long you want the password to be"
   );
-  var isLower = confirm("Do you want lower case letters?");
-  var isUpper = confirm("Do you want upper case letters?");
-  var isSpecial = confirm("Do you want special characters?");
-  var isNumber = confirm("Do you want numbers?");
 
   //if the password is not within criteria send an alert to the user to the mistake
   if (passLength < 8 || passLength > 128) {
     alert("Please enter a valid length");
+  } 
+  //checks if the password length entered is a number
+  else if (isNaN(passLength)) {
+    alert("Please enter a valid number");
   } else {
+    //prompts the user for what characters they want
+    var isLower = confirm("Do you want lower case letters?");
+    var isUpper = confirm("Do you want upper case letters?");
+    var isSpecial = confirm("Do you want special characters?");
+    var isNumber = confirm("Do you want numbers?");
     //if the user did not select type of characters then alert the user with the mistake
     if (!isLower && !isUpper && !isSpecial && !isNumber) {
       alert("Please specify at least one type of character");
     }
 
     //Adds what characters the user wants into an array to be selected later on
+    //This also adds 1 character in at the beginning of the password to guarantee that character apears in the password
     else {
       if (isLower) {
-        //adds lowercase
+        //adds lowercase to the choices and put a lowercase letter in
         characterChoice.push("l");
-        //workingPass = workingPass + lowerCase(Math.random()*lowerCase.length);
+        workingPass =
+          workingPass + lowerCase[Math.floor(Math.random() * lowerCase.length)];
       }
       if (isUpper) {
-        //adds uppercase
+        //adds uppercase to the choices and put an uppercase letter in
         characterChoice.push("u");
-        // workingPass = workingPass + upperCase(Math.random()*upperCase.length);
+        workingPass =
+          workingPass + upperCase[Math.floor(Math.random() * upperCase.length)];
       }
       if (isSpecial) {
-        //adds special characters
+        //adds special characters to the choices and put a special character in
         characterChoice.push("s");
-        //workingPass = workingPass + numbers(Math.random()*numbers.length);
+        workingPass =
+          workingPass +
+          specialChar[Math.floor(Math.random() * specialChar.length)];
       }
       if (isNumber) {
-        //adds numbers
+        //adds numbers to the choices and puts a number in
         characterChoice.push("n");
-        // workingPass = workingPass + numbers(Math.random()*numbers.length);
+        workingPass =
+          workingPass + numbers[Math.floor(Math.random() * numbers.length)];
       }
 
-      //for loop for adding characters in
-      for (var i = 0; i < passLength; i++) {
+      //for loop for adding characters in ot the working password
+      for (var i = characterChoice.length; i < passLength; i++) {
         //get either a u,l,s,n random from the characterchoice array from above
         var selector =
           characterChoice[Math.floor(Math.random() * characterChoice.length)];
-
         //adds a lowercase letter
         if (selector === "l") {
           workingPass =
-            workingPass + lowerCase[Math.floor(Math.random() * lowerCase)];
-          console.log("lowercase");
+            workingPass +
+            lowerCase[Math.floor(Math.random() * lowerCase.length)];
         }
         //adds an uppercase letter
         else if (selector === "u") {
           workingPass =
-            workingPass + upperCase[Math.floor(Math.random() * upperCase)];
-          console.log("uppercase");
+            workingPass +
+            upperCase[Math.floor(Math.random() * upperCase.length)];
         }
         //adds a special character
         else if (selector === "s") {
           workingPass =
-            workingPass + specialChar[Math.floor(Math.random() * specialChar)];
-          console.log("specialChar");
+            workingPass +
+            specialChar[Math.floor(Math.random() * specialChar.length)];
         }
         //adds a number character
         else if (selector === "n") {
           workingPass =
-            workingPass + numbers[Math.floor(Math.random() * numbers)];
-          console.log("numbers");
+            workingPass + numbers[Math.floor(Math.random() * numbers.length)];
         }
-        //if for some reason it gets to this point log an error becuase it should never happen
+        //if for some reason it gets to this point, log an error becuase it should never happen
         else {
           console.log("ERROR");
         }
-        //console.log(i);
       }
     }
   }
-  //console.log(characterChoice);
-  //console.log(workingPass);
+  //returns the working password
+  return workingPass;
 }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@ This project is to generate a passcode given a certain criteria prompted by the 
 
 Clicking the button will pormpt the user for a certain length between 8 and 128, and whether the user would like to have either lowercase, uppercase, special characters, or numbers in the password.
 
-The program will then create a random password with all the characters specified.
+The program will then create a random password with all the characters specified at the given length.
